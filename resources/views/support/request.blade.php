7f381b9bc6c4768c8b1ccebc305af05ccfa04aec
Ahmad <ahmedelmalah@hotmail.com>
2016-08-01 15:25:15 +0200

    Request blade is now able to load messages

--- resources/views/support/request.blade.php
+++ resources/views/support/request.blade.php
@@ -8,120 +8,73 @@
     <div class="row">
                   <ul class="timeline">
                     <!-- timeline time label -->
+                    <?php
+                      $lastdate = '';
+                    ?>
                     @foreach($messages as $message)
-                    <li class="time-label">
-                          <span class="bg-red">
-                            {{$message->text}}
-                          </span>
-                    </li>
-                    @endforeach
-                    <li class="time-label">
-                          <span class="bg-red">
-                            July 30, 2016
-                          </span>
-                    </li>
-                    <!-- /.timeline-label -->
 
-                    <!-- timeline item -->
-                    <li>
-                      <i class="fa fa-envelope bg-blue"></i>
-                      <div class="timeline-item">
-                        <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
-                        <h3 class="timeline-header"><a href="#">Maher</a> started a new ticket</h3>
-                      </div>
-                    </li>
-                    <!-- END timeline item -->
-
-                    <!-- timeline item -->
-                    <li>
-                      <i class="fa fa-comments bg-yellow"></i>
-                      <div class="timeline-item">
-                        <span class="time"><i class="fa fa-clock-o"></i> 12:10</span>
-                        <h3 class="timeline-header"><a href="#">Maher</a> said:</h3>
-                        <div class="timeline-body">
-                          Please I can't use PGX application
-                          I've tried to re-install it but I faild
-                          thanks
-                        </div>
-                      </div>
-                    </li>
-                    <!-- END timeline item -->
-
-                    <!-- timeline item -->
-                    <li>
-                      <i class="fa fa-user bg-aqua"></i>
-
-                      <div class="timeline-item">
-                        <span class="time"><i class="fa fa-clock-o"></i> 12:30</span>
-                        <h3 class="timeline-header no-border"><a href="#">Customer Survices</a> has read your meassage</h3>
-                      </div>
-                    </li>
-                    <!-- END timeline item -->
-
-                    <!-- timeline item -->
-                    <li>
-                      <i class="fa fa-comments bg-yellow"></i>
-                      <div class="timeline-item">
-                        <span class="time"><i class="fa fa-clock-o"></i> 12:40</span>
-                        <h3 class="timeline-header"><a href="#">Customer Survices</a> said:</h3>
-                        <div class="timeline-body">
-                          Good morning Dr. maher
-                          Could you please provide us with a screenshot to the problem
-                          Thanks
-                        </div>
-                      </div>
-                    </li>
-                    <!-- END timeline item -->
-
-                    <!-- timeline item -->
-                    <li>
-                      <i class="fa fa-bell-o bg-aqua"></i>
-                      <div class="timeline-item">
-                        <span class="time"><i class="fa fa-clock-o"></i> 12:40</span>
-                        <h3 class="timeline-header"><a href="#">Customer Survices</a> has sent a reminder:</h3>
-
-                      </div>
-                    </li>
-                    <!-- END timeline item -->
-
-                    <!-- timeline item -->
-                    <li>
-                      <i class="fa fa-camera bg-purple"></i>
-
-                      <div class="timeline-item">
-                        <span class="time"><i class="fa fa-clock-o"></i> 12:50</span>
-
-                        <h3 class="timeline-header"><a href="#">Maher</a> uploaded new photos</h3>
-
-                        <div class="timeline-body">
-                          <img src="http://placehold.it/150x100" alt="..." class="margin">
-                        </div>
-                      </div>
-                    </li>
-
-
-                    <!-- timeline time label -->
+                    <!-- Date item -->
+                    @if(date("F j, Y", strtotime($message->created_at)) !== $lastdate)
                     <li class="time-label">
                           <span class="bg-green">
-                            July 31, 2016
+                            {{date("F j, Y", strtotime($message->created_at))}}
                           </span>
                     </li>
-                    <!-- /.timeline-label -->
+                    <?php
+                      $lastdate = date("F j, Y", strtotime($message->created_at));
+                    ?>
+                    @endif
+                    <!-- End of date item -->
 
                     <!-- timeline item -->
+                    <?php
+                      switch (trim(strtolower($message->text))) {
+                        case '_new_':
+                          $title = ' started a new Ticket';
+                          $text = '';
+                          $icon = 'fa fa-envelope bg-blue';
+                          break;
+
+                        case 'read':
+                          $title = ' has read the previous message(s)';
+                          $text = '';
+                          $icon = 'fa fa-eye bg-aqua';
+                          break;
+
+                        case 'reminder':
+                          $title = ' has sent a reminder';
+                          $text = '';
+                          $icon = 'fa fa-bell-o bg-aqua';
+                          break;
+
+                        default:
+                          $title = ' said :';
+                          $text = $message->text;
+                          $icon = 'fa fa-comments bg-yellow';
+
+                          if ($message->pic){
+                            $title = ' uploaded a photo';
+                            $text = $message->text;
+                            $icon = 'fa fa-camera bg-purple';
+                          }
+                          break;
+                      }
+                    ?>
                     <li>
-                      <i class="fa fa-comments bg-yellow"></i>
+                      <i class="{{$icon}}"></i>
                       <div class="timeline-item">
-                        <span class="time"><i class="fa fa-clock-o"></i> 09:10</span>
-                        <h3 class="timeline-header"><a href="#">Customer Survices</a> said: </h3>
+                        <span class="time"><i class="fa fa-clock-o"></i> {{date("h:i A", strtotime($message->created_at))}}</span>
+                        <h3 class="timeline-header"><a href="#">{{$message->name}}</a> {{$title}}</h3>
+                        @if($text)
                         <div class="timeline-body">
-                          We have solved the problem, you can now try again
-                          Thank you for reporting the problem
+                          {{$text}}
                         </div>
+                        @endif
                       </div>
                     </li>
                     <!-- END timeline item -->
 
+                    @endforeach
                   </ul>
     </div>
 </div>
