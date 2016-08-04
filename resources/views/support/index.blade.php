155e66fa729871454b3b9de8e05e481ca32ce109
Ahmad <ahmedelmalah@hotmail.com>
2016-08-04 15:50:42 +0200

    Index Preparing

--- resources/views/support/index.blade.php
+++ resources/views/support/index.blade.php
@@ -3,13 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-body">
-
-                </div>
-            </div>
-        </div>
+      
     </div>
 </div>
 @endsection
