42ae33e40bed7fdec12f5ca278bbbf9a09b6fd1d
Ahmad <ahmedelmalah@hotmail.com>
2016-08-07 14:43:34 +0200

    Requests Preview Using Angular

--- resources/views/support/index.blade.php
+++ resources/views/support/index.blade.php
@@ -1,9 +1,82 @@
 @extends('layout.main')
 
 @section('content')
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
 <div class="container">
-    <div class="row">
-      
-    </div>
+  <div class="row">
+    <div class="col-md-12"><br /></div>
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Company</th>
+          <th>Problem Type</th>
+          <th>Date</th>
+          <th>Status</th>
+        </tr>
+        <tr>
+          <th></th>
+          <th>
+            <select class="form-control"  ng-model="search_company">
+              <option value="">- All -</option>
+              <option ng-repeat="row in reqs | unique:'company'" value="[[row.company.name]]">[[row.company.name]]</option>
+            </select>
+          </th>
+          <th>
+            <select class="form-control"  ng-model="search_type">
+              <option value="">- All -</option>
+              <option ng-repeat="row in reqs | unique:'type'" value="[[row.type]]">[[row.type]]</option>
+            </select>
+          </th>
+          <th></th>
+          <th>
+            <select class="form-control"  ng-model="search_status">
+              <option value="">- All -</option>
+              <option ng-repeat="row in reqs | unique:'status'" value="[[row.status]]">[[row.status]]</option>
+            </select>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr ng-repeat="row in reqs | filter:{company:{name: search_company}	, type: search_type,status: search_status}">
+          <td><a href="request/2/[[row.id]]">[[row.id]]<a></td>
+          <td>[[row.company.name]]</td>
+          <td>[[row.type]]</td>
+          <td>[[row.created_at]]</td>
+          <td>[[row.status]]</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </div>
+<script>
+var app=angular.module('myApp', []);
+
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+});
+
+app.filter('unique', function() {
+   return function(collection, keyname) {
+      var output = [],
+          keys = [];
+
+      angular.forEach(collection, function(item) {
+          var key = item[keyname];
+          if(keys.indexOf(key) === -1) {
+              keys.push(key);
+              output.push(item);
+          }
+      });
+
+      return output;
+   };
+});
+
+app.controller('gridCtrl', function($scope){
+  $scope.reqs = {!! $reqs !!};
+  $scope.reqs_disp = [].concat($scope.reqs);
+});
+</script>
 @endsection
