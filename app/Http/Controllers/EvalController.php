feaeeb1015719e1907ccaab8ab17622246544887
Ahmad <ahmedelmalah@hotmail.com>
2016-11-30 14:00:50 +0200

    adding charts feature

--- app/Http/Controllers/EvalController.php
+++ app/Http/Controllers/EvalController.php
@@ -9,48 +9,74 @@ use DB;
 
 class EvalController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
+  /**
+  * Create a new controller instance.
+  *
+  * @return void
+  */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
+  /**
+  * Show the application dashboard.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index($company, $uid)
+  {
+    $data['eval_sessions'] = DB::table('eval_sessions')->where(['company' => $company, 'rep_id' => $uid])
+    //->leftJoin('User', 'SuperReps.rid', '=', 'User.uid')
+    //->select('User.uid as id' , 'User.FullName as name')
+    ->get();
+    $data['company'] = $company;
+    $data['uid'] = $uid;
+    return view('evals', $data);
+  }
+
+  public function getEval($company, $evalID)
+  {
+    $data['eval_ans'] = DB::table('eval_ans')->where(['eval_session_id' => $evalID])
+    ->leftJoin('eval_qus', 'eval_ans.q_id', '=', 'eval_qus.id')
+    ->leftJoin('eval_cats', 'eval_qus.cat_id', '=', 'eval_cats.id')
+    ->select('eval_cats.name as cat' , 'eval_qus.name' , 'eval_ans.answer')
+    ->orderBy('eval_qus.cat_id', 'asc')
+    ->orderBy('eval_qus.id', 'asc')
+
+    ->get();
+
+    $data['eval_total'] = DB::select('SELECT sum(answer) as c
+    FROM `eval_ans`
+    Join eval_qus on eval_ans.q_id= eval_qus.id and eval_qus.cat_id not in (8,9)
+    WHERE eval_session_id=' . $evalID);
+    $data['company'] = $company;
+    return view('eval', $data);
+  }
+
+  public function getEvalCharts($company){
+    $data['company'] = $company;
+
+    if(isset($_GET['region']) && isset($_GET['month']) && isset($_GET['year'])){
+      $sql = "SELECT eval_qus.cat_id, eval_cats.name , COUNT(answer) as totals,
+      Sum(Case When answer = 3 Then 1 Else 0 End) as goods,
+      Sum(Case When answer = 1 Then 1 Else 0 End) as nis
+
+      FROM `eval_sessions`
+      JOIN nichepha_tabukdb.User user on eval_sessions.rep_id=user.uid
+      join eval_ans on eval_sessions.id = eval_ans.eval_session_id
+      join eval_qus on eval_ans.q_id = eval_qus.id
+      JOIN eval_cats on eval_qus.cat_id = eval_cats.id
+      where company=1 and user.region={$_GET['region']}
+      and month(date) = {$_GET['month']}
+      and year(date) = {$_GET['year']}
+      and cat_id < 8
+      GROUP BY cat_id";
+      $data['cats_data'] = DB::select($sql);
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index($company, $uid)
-    {
-        $data['eval_sessions'] = DB::table('eval_sessions')->where(['company' => $company, 'rep_id' => $uid])
-        //->leftJoin('User', 'SuperReps.rid', '=', 'User.uid')
-        //->select('User.uid as id' , 'User.FullName as name')
-        ->get();
-        $data['company'] = $company;
-        $data['uid'] = $uid;
-        return view('evals', $data);
-    }
 
-    public function getEval($company, $evalID)
-    {
-        $data['eval_ans'] = DB::table('eval_ans')->where(['eval_session_id' => $evalID])
-        ->leftJoin('eval_qus', 'eval_ans.q_id', '=', 'eval_qus.id')
-        ->leftJoin('eval_cats', 'eval_qus.cat_id', '=', 'eval_cats.id')
-        ->select('eval_cats.name as cat' , 'eval_qus.name' , 'eval_ans.answer')
-        ->orderBy('eval_qus.cat_id', 'asc')
-        ->orderBy('eval_qus.id', 'asc')
-
-        ->get();
-
-        $data['eval_total'] = DB::select('SELECT sum(answer) as c
-                                          FROM `eval_ans`
-                                          Join eval_qus on eval_ans.q_id= eval_qus.id and eval_qus.cat_id not in (8,9)
-                                          WHERE eval_session_id=' . $evalID);
-        $data['company'] = $company;
-        return view('eval', $data);
-    }
+    $data['countries'] = DB::connection('tabuk')->select("SELECT * from regions where regid>0");
+    return view('evalCharts', $data);
+  }
 }
---
+++ resources/views/evalCharts.blade.php
@@ -0,0 +1,131 @@
+@extends('layout.main')
+
+@section('content')
+<style>
+/* centered columns styles */
+.row-centered {
+    text-align:center;
+    margin-bottom: 50px;
+}
+.col-centered {
+    display:inline-block;
+    float:none;
+    /* reset the text-align */
+    text-align:left;
+    /* inline-block space fix */
+    margin-right:-4px;
+}
+</style>
+<div class="container">
+  @include('others.print_buttons')
+
+  <div class="row row-centered">
+      <form class="form-inline">
+
+        <div class="form-group">
+          <label>Country : </label>
+          <select class="form-control" id="country">
+            <option selected disabled>Choose Here</option>
+            @foreach($countries as $country)
+            <option value="{{$country->regid}}">{{$country->region}}</option>
+            @endforeach
+          </select>
+        </div>
+        <div class="form-group">
+          <label>When : </label>
+          <select class="form-control" id="year">
+            <option selected disabled>Choose here</option>
+            <option value="2016">2016</option>
+          </select>
+          <select class="form-control" id="month">
+            <option selected disabled>Choose here</option>
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+        </div>
+        <input type="button" class="btn btn-primary" onclick="preview()" value="Preview">
+
+      </form>
+  </div>
+  <div class="row">
+    <div class="col-md-10 col-md-offset-1">
+
+      @if(isset($cats_data))
+      @foreach($cats_data as $cat_data)
+      <div class="row">
+        <div class="col-md-6">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <h3 class="panel-title">{{$cat_data->name}} skill at a glance</h3>
+            </div>
+            <div class="panel-body">
+              <dl class="dl-horizontal">
+                <dt>Need Improvments</dt><dd> {{$cat_data->nis}} </dd>
+                <dt>Good</dt><dd> {{$cat_data->goods}} </dd>
+
+                <dt>Need Improvments (%)</dt><dd> {{round($cat_data->nis * 100 / $cat_data->totals)}}% </dd>
+                <dt>Good(%) </dt><dd> {{round($cat_data->goods * 100 / $cat_data->totals)}}% </dd>
+
+                <dt>Total Answers</dt><dd> {{$cat_data->totals}} </dd>
+
+              </dl>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <canvas id="myChart{{$cat_data->cat_id}}"></canvas>
+        </div>
+      </div>
+      @endforeach
+      @endif
+    </div>
+  </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
+<script>
+var option = {
+  animation: {
+    duration:5000
+  },
+  responsive:false,
+  maintainAspectRatio: false
+};
+
+@if(isset($cats_data))
+@foreach($cats_data as $cat_data)
+var ctx = document.getElementById("myChart{{$cat_data->cat_id}}").getContext('2d');
+
+var myChart = new Chart(ctx, {
+  type: 'pie',
+  option : option,
+  data: {
+    labels: ["Need Improvment", "Good"],
+    datasets: [{
+      backgroundColor: [
+        "#34495e",
+        "#2ecc71"
+      ],
+      data: [{{$cat_data->nis}}, {{$cat_data->goods}}]
+    }]
+  }
+});
+@endforeach
+@endif
+</script>
+<script>
+function preview(){
+  window.location = '1?region=' + $("#country" ).val() + '&year=' + $("#year" ).val() + '&month=' + $("#month" ).val();
+}
+</script>
+@endsection
