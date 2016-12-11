7a2ef43344c10927737a1cfff7bb1a9a29e149fe
Ahmad <ahmedelmalah@hotmail.com>
2016-12-11 19:54:15 +0200

    Allowing Supervisor and Rep Filtering

--- app/Http/Controllers/EvalController.php
+++ app/Http/Controllers/EvalController.php
@@ -26,13 +26,13 @@ class EvalController extends Controller
   */
   public function index($company, $uid)
   {
-    $data['eval_sessions'] = DB::table('eval_sessions')->where(['company' => $company, 'rep_id' => $uid])
-    //->leftJoin('User', 'SuperReps.rid', '=', 'User.uid')
-    //->select('User.uid as id' , 'User.FullName as name')
-    ->get();
-    $data['company'] = $company;
-    $data['uid'] = $uid;
-    return view('evals', $data);
+      $data['eval_sessions'] = DB::table('eval_sessions')->where(['company' => $company, 'rep_id' => $uid])
+      //->leftJoin('User', 'SuperReps.rid', '=', 'User.uid')
+      //->select('User.uid as id' , 'User.FullName as name')
+      ->get();
+      $data['company'] = $company;
+      $data['uid'] = $uid;
+      return view('evals', $data);
   }
 
   public function getEval($company, $evalID)
@@ -55,9 +55,14 @@ class EvalController extends Controller
   }
 
   public function getEvalCharts($company){
-    $data['company'] = $company;
+    if(!isset($_GET['ajax']) || $_GET['ajax'] != "1"){
 
+    $data['company'] = $company;
     if(isset($_GET['region']) && isset($_GET['month']) && isset($_GET['year'])){
+      $supervisor_id = $_GET['supervisor'];
+      $rep_id = $_GET['rep'];
+      if ($supervisor_id == 'null') $supervisor_id = 'eval_sessions.supervisor_id';
+      if ($rep_id == 'null') $rep_id = 'eval_sessions.rep_id';
       $sql = "SELECT eval_qus.cat_id, eval_cats.name , COUNT(answer) as totals,
       Sum(Case When answer = 3 Then 1 Else 0 End) as goods,
       Sum(Case When answer = 1 Then 1 Else 0 End) as nis
@@ -70,13 +75,24 @@ class EvalController extends Controller
       where company=1 and user.region={$_GET['region']}
       and month(date) = {$_GET['month']}
       and year(date) = {$_GET['year']}
+      and eval_sessions.supervisor_id	= $supervisor_id
+      and eval_sessions.rep_id = $rep_id
       and cat_id < 8
       GROUP BY cat_id";
       $data['cats_data'] = DB::select($sql);
     }
-
-
     $data['countries'] = DB::connection('tabuk')->select("SELECT * from regions where regid>0");
+
+    //User data (as texts)
+    $data['country']  = DB::connection('tabuk')->select("SELECT * from regions where regid={$_GET['region']}");
+    $data['supervisor']  = DB::connection('tabuk')->select("SELECT * from regions where regid={$_GET['region']}");
+    $data['rep']  = DB::connection('tabuk')->select("SELECT * from regions where regid={$_GET['region']}");
     return view('evalCharts', $data);
+  }else{
+    //Ajax Requests
+    $data['reps'] = DB::connection('tabuk')->select("SELECT uid as 'value',FullName as 'text' FROM `User` where active=1 and JobTitle='Sales Rep' and region={$_GET['region']}");
+    $data['sups'] = DB::connection('tabuk')->select("SELECT uid as 'value',FullName as 'text' FROM `User` where active=1 and JobTitle='SuperVisor' and region={$_GET['region']}");
+    return $data;
+  }
   }
 }
