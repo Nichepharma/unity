2e50185e53c7c343e17ad388b10c0767ec797ef8
Ahmad <ahmedelmalah@hotmail.com>
2017-01-22 12:46:00 +0200

    Adding time to fillable array

--- app/EvalSession.php
+++ app/EvalSession.php
@@ -10,5 +10,5 @@ class EvalSession extends Model
      * Get the phone record associated with the user.
      */
      protected $table = 'eval_sessions';
-     protected $fillable = array('company', 'supervisor_id', 'rep_id', 'date', 'supervisor_signature', 'rep_signature');
+     protected $fillable = array('company', 'supervisor_id', 'rep_id', 'time','date', 'supervisor_signature', 'rep_signature');
 }
